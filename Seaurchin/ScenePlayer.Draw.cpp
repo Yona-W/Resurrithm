fd7d7889c736b997eb6682e1d62e7702d3414493
amenoshita-429 <44079307+amenoshita-429@users.noreply.github.com>
2019-08-21 22:23:28 +0900

    スキンへ移譲したScenePlayerの効果音に関する処理を上書きしていたバグを修正
    見逃してた

--- Seaurchin/ScenePlayer.Draw.cpp
+++ Seaurchin/ScenePlayer.Draw.cpp
@@ -41,37 +41,7 @@ void ScenePlayer::LoadResources()
 	animeSlideLoop = dynamic_cast<SAnimatedImage*>(resources["EffectSlideLoop"]);
 	animeAirAction = dynamic_cast<SAnimatedImage*>(resources["EffectAirAction"]);
 
-	soundTap = dynamic_cast<SSound*>(resources["SoundTap"]);
-	soundExTap = dynamic_cast<SSound*>(resources["SoundExTap"]);
-	soundFlick = dynamic_cast<SSound*>(resources["SoundFlick"]);
-	soundAir = dynamic_cast<SSound*>(resources["SoundAir"]);
-	soundAirDown = dynamic_cast<SSound*>(resources["SoundAirDown"]);
-	soundAirAction = dynamic_cast<SSound*>(resources["SoundAirAction"]);
-	soundAirLoop = dynamic_cast<SSound*>(resources["SoundAirHoldLoop"]);
-	soundSlideLoop = dynamic_cast<SSound*>(resources["SoundSlideLoop"]);
-	soundHoldLoop = dynamic_cast<SSound*>(resources["SoundHoldLoop"]);
-	soundSlideStep = dynamic_cast<SSound*>(resources["SoundSlideStep"]);
-	soundHoldStep = dynamic_cast<SSound*>(resources["SoundHoldStep"]);
-	soundMetronome = dynamic_cast<SSound*>(resources["Metronome"]);
-
 	const auto setting = manager->GetSettingManagerUnsafe()->GetSettingInstanceUnsafe();
-	if (soundHoldLoop) soundHoldLoop->SetLoop(true);
-	if (soundSlideLoop) soundSlideLoop->SetLoop(true);
-	if (soundAirLoop) soundAirLoop->SetLoop(true);
-	if (soundTap) soundTap->SetVolume(setting->ReadValue("Sound", "VolumeTap", 1.0));
-	if (soundExTap) soundExTap->SetVolume(setting->ReadValue("Sound", "VolumeExTap", 1.0));
-	if (soundFlick) soundFlick->SetVolume(setting->ReadValue("Sound", "VolumeFlick", 1.0));
-	if (soundAir) soundAir->SetVolume(setting->ReadValue("Sound", "VolumeAir", 1.0));
-	if (soundAirDown) soundAirDown->SetVolume(setting->ReadValue("Sound", "VolumeAir", 1.0));
-	if (soundAirAction) soundAirAction->SetVolume(setting->ReadValue("Sound", "VolumeAirAction", 1.0));
-	if (soundAirLoop) soundAirLoop->SetVolume(setting->ReadValue("Sound", "VolumeAirAction", 1.0));
-	if (soundHoldLoop) soundHoldLoop->SetVolume(setting->ReadValue("Sound", "VolumeHold", 1.0));
-	if (soundSlideLoop) soundSlideLoop->SetVolume(setting->ReadValue("Sound", "VolumeSlide", 1.0));
-	if (soundHoldStep) soundHoldStep->SetVolume(setting->ReadValue("Sound", "VolumeHold", 1.0));
-	if (soundSlideStep) soundSlideStep->SetVolume(setting->ReadValue("Sound", "VolumeSlide", 1.0));
-
-	if (soundMetronome) soundMetronome->SetVolume(setting->ReadValue("Sound", "VolumeTap", 1.0));
-	else soundMetronome = soundTap;
 
 	vector<toml::Value> scv = { 0, 200, 255 };
 	vector<toml::Value> aajcv = { 128, 255, 160 };
